c0693294cb1e349c6d5254bf872dff27d7ba51d5
Carlos F Arboleda G <arbofercho@gmail.com>
2020-09-13 10:36:05 -0500

    Improve router implementation for automatic files load using glob

--- src/Router.ts
+++ src/Router.ts
@@ -1,19 +1,31 @@
 import * as Hapi from '@hapi/hapi';
+import { glob } from 'glob';
+import * as Path from 'path';
 import Database from './datasource/database';
-import DatabasePingRoute from './modules/shared/routes/DatabasePingRoute';
-import PopulateSongsRoute from './modules/songs/routes/PopulateRoute';
-import GetSongsRoute from './modules/songs/routes/GetSongsRoute';
-import GetSongByIdRoute from './modules/songs/routes/GetSongByIdRoute';
 
 export default class Router {
     public static async loadRoutes(server: Hapi.Server, database: Database<any>): Promise<any> {
-        console.info('Router - Start adding routes');
+        try {
+            console.info('Router - Start adding routes');
+            glob(
+                'modules/*/routes/*.+(js|ts)', { cwd: __dirname },
+                (error: Error | null, files: Array<string>) => {
+                    if (error) {
+                        console.error('Router - Error loading route files');
+                        return;
+                    }
 
-        await new DatabasePingRoute().register(server, database);
-        await new PopulateSongsRoute().register(server, database);
-        await new GetSongsRoute().register(server, database);
-        await new GetSongByIdRoute().register(server, database);
-
-        console.info('Router - Finish adding routes');
+                    console.info(`Router - Found ${files.length} routes`);
+                    files.forEach(async (filePath: string) => {
+                        console.info(Path.relative('./', Path.join(__dirname, filePath)));
+                        const RouteClass = require(Path.join(__dirname, filePath)).default;
+                        await new RouteClass().register(server, database);
+                    });
+                }
+            );
+            console.info('Router - Finish adding routes');
+        } catch (error) {
+            console.error('Router - Error loading route files', error);
+        }
     }
 }
